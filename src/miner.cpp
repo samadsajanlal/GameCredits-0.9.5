978251e36e1cf8abf30fd1017b34bd1db6f69ebe
Samad Sajanlal <samad.sajanlal@gmail.com>
2020-02-17 16:06:43 -0600

    changes

--- src/miner.cpp
+++ src/miner.cpp
@@ -185,9 +185,6 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     coinbaseTx.vin.resize(1);
     coinbaseTx.vin[0].prevout.SetNull();
     coinbaseTx.vout.resize(1);
-    LogPrintf("hello createBlock()");
-    LogPrintf("Now at the following block: %u\n", nHeight);
-    LogPrintf("PayDevelopersBlock: %u\n", chainparams.PayDevelopersBlock());
     if(nHeight == chainparams.PayDevelopersBlock()) {
 		// create developers pay to amount
 		//payto: chainParams.getPayToDevAddress() 
@@ -206,7 +203,7 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     pblocktemplate->vchCoinbaseCommitment = GenerateCoinbaseCommitment(*pblock, pindexPrev, chainparams.GetConsensus());
     pblocktemplate->vTxFees[0] = -nFees;
     uint64_t nSerializeSize = GetSerializeSize(*pblock, SER_NETWORK, PROTOCOL_VERSION);
-    LogPrintf("CreateNewBlock(): total size: %u block weight: %u txs: %u fees: %ld sigops %d\n", nSerializeSize, GetBlockWeight(*pblock), nBlockTx, nFees, nBlockSigOpsCost);
+    LogPrintf("CreateNewBlock(): t1tal size: %u block weight: %u txs: %u fees: %ld sigops %d\n", nSerializeSize, GetBlockWeight(*pblock), nBlockTx, nFees, nBlockSigOpsCost);
 
     // Fill in header
     pblock->hashPrevBlock  = pindexPrev->GetBlockHash();
